23b7e92368c4b4dcc8e6feeb8ee8c3a8cf96fdc3
Rare Bird, Inc <developer@rarebirdinc.com>
2016-06-21 15:05:22 +0000

    Changed html class no-js to mod-no-js, which is required for Modernizr 3 to alter

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
+<html class="mod-no-js" <?php language_attributes(); ?>>
 <head>
 	<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	* Built with love by...
