b2dd63ea1ed2f951129e9d54de5eaf1ad836f111
Ubuntu <ubuntu@rarebird.io>
2017-10-03 20:53:38 +0000

    Update for Perch v0.8.1, bringing some wrapping markup in line with latest Takeoff standards, setting forth more explicit LESS files for the developer to work in, version updating.

--- searchform-nav.php
+++ searchform-nav.php
@@ -4,11 +4,13 @@
 	</div>
 	<div class="global-search-overlay__inner full-page-backdrop__inner">
 		<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" id="global-search-overlay-form" autocomplete="off" role="search">
-			<input id="global-search-overlay__input" class="global-search-overlay__input input-field mousetrap" type="search" name="global-search-overlay__input" size="20" maxlength="100" placeholder="Enter search keyword(s)">
+			<div class="global-search-overlay-input-wrapper">
+				<input id="global-search-overlay__input" class="global-search-overlay__input input-field mousetrap" type="search" name="global-search-overlay__input" size="20" maxlength="100" placeholder="Enter search keyword(s)">
 
-			<button id="global-search-overlay__submit" class="global-search-overlay__submit icon-search button button--smaller" type="submit" title="Click here to search.">
-				<span class="sr-only">Search</span>
-			</button>
+				<button id="global-search-overlay__submit" class="global-search-overlay__submit icon-search button button--smaller" type="submit" title="Click here to search.">
+					<span class="sr-only">Search</span>
+				</button>
+			</div>
 		</form>
 
 		<div class="global-search-overlay__actions">
