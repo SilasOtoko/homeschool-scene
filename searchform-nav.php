8b7c0ec7b0be60523fd235686e8101a2e4ddc234
Ubuntu <ubuntu@rarebird.io>
2018-02-27 17:13:05 +0000

    Search form fix

--- searchform-nav.php
+++ searchform-nav.php
@@ -2,7 +2,7 @@
 	<div class="global-search-overlay__inner full-page-backdrop__inner">
 		<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" id="global-search-overlay-form" autocomplete="off" role="search">
 			<div class="global-search-overlay-input-wrapper">
-				<input id="global-search-overlay__input" class="global-search-overlay__input input-field mousetrap" type="search" name="global-search-overlay__input" size="20" maxlength="100" placeholder="Enter search keyword(s)">
+				<input id="global-search-overlay__input" class="global-search-overlay__input input-field mousetrap" type="search" name="s" size="20" maxlength="100" placeholder="Enter search keyword(s)">
 
 				<button id="global-search-overlay__submit" class="global-search-overlay__submit icon-search button button--smaller" type="submit" title="Click here to search.">
 					<span class="sr-only">Search</span>
