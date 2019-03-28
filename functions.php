4fcd4cdab83bed742e8c0878e78d95800441b99c
Josiah Schaefer <josiah+gitlab@rarebirdinc.com>
2019-03-28 09:33:59 -0400

    updating default pages

--- functions.php
+++ functions.php
@@ -57,3 +57,23 @@ function goshawk_theme_setup() {
      */
     add_theme_support( 'title-tag' );
 }
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
--- index.php
+++ index.php
@@ -5,42 +5,59 @@
 		<div class="main-content-column col-xs-12">
 			<section class="main-content">
 
-				<?php if( have_posts() ): ?>
+				<?php
+					if ( have_posts() ) :
 
-        	<?php while( have_posts() ): the_post(); ?>
-
-
-						<div id="post-<?php the_ID(); ?>" <?php post_class("post-content-wrapper"); ?>>
+						if ( is_home() && ! is_front_page() ) :
+							?>
 
 							<div class="page-intro">
 
-	              <div class="entry-header text-wrapper">
+			          <div class="entry-header text-wrapper">
 
-	                <h1 class="page-main-heading entry-title"><?php the_title(); ?></h1>
+			            <h1 class="page-main-heading entry-title"><?php single_post_title(); ?></h1>
+			              
+			          </div>
 
-	                <?php if( function_exists( 'get_field' ) ): ?>
+			          <?php 
 
-	                  <?php the_field( 'page_intro' ); ?>
+				          $posts_page_id = get_option( 'page_for_posts' ); 
+				          $posts_page = get_post( $posts_page_id );
 
-	                <?php endif; ?>
-	                  
-	              </div>
+				        ?>
 
-	            </div>
+				        <?php if( $posts_page->post_content ): ?>
 
-							<div class="entry-content text-wrapper">
+				          <p><?php echo $posts_page->post_content; ?></p>
 
-								<?php the_content(); ?>
+				        <?php endif; ?>
 
-								<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'blankslate' ) . '&after=</div>'); ?>
-								<?php edit_post_link( __( 'Edit', 'blankslate' ), '<div class="edit-link">', '</div>' ); ?>
-							</div>
+			        </div>
 
-						</div>
+							<?php
+						endif;
 
-					<?php endwhile; ?>
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
 
-				<?php endif; ?>
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content/content', get_post_type() );
+
+						endwhile;
+
+						the_posts_navigation();
+
+					else :
+
+						get_template_part( 'template-parts/content/content', 'none' );
+
+					endif;
+				?>
 
 			</section>
 		</div>
