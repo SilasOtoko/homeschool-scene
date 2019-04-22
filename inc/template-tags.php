8e3848157c48fc3b1bc20cff37cbd6f07900bbdb
Josiah Schaefer <josiah+gitlab@rarebirdinc.com>
2019-04-22 16:54:52 -0400

    removing edit link from search results

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -72,23 +72,6 @@ if ( ! function_exists( 'goshawk_entry_footer' ) ) :
 				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'goshawk' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
-
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'goshawk' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
 	}
 endif;
 
