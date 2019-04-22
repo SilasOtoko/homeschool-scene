d243a88808e2c7c6379fa7ba9b2b09e5f48ab90b
Josiah Schaefer <josiah+gitlab@rarebirdinc.com>
2019-04-22 14:33:30 -0400

    fixing sidebar hover and adding dashItAll function

--- custom/scripts.php
+++ custom/scripts.php
@@ -41,3 +41,15 @@ function goshawk_include_custom_jquery() {
 
 }
 add_action('wp_enqueue_scripts', 'goshawk_include_custom_jquery');
+
+function dashItAll($string) {
+  //Lower case everything
+  $string = strtolower($string);
+  //Make alphanumeric (removes all other characters)
+  $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+  //Clean up multiple dashes or whitespaces
+  $string = preg_replace("/[\s-]+/", " ", $string);
+  //Convert whitespaces and underscore to dash
+  $string = preg_replace("/[\s_]/", "-", $string);
+  return $string;
+}
