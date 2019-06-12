8ab5849b77e270ab879766f7c198435e49f40b19
Josiah Schaefer <josiah+gitlab@rarebirdinc.com>
2019-06-12 10:41:39 -0400

    restructuring directory and changing up gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,35 +9,36 @@ import autoprefixer from 'autoprefixer';
 import imagemin from 'gulp-imagemin';
 import del from 'del';
 import webpack from 'webpack-stream';
-import merge2 from 'merge2';
+import browserSync from "browser-sync";
+import zip from "gulp-zip";
+import info from "./package.json";
 
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
-  return merge2(
-    src('src/less/style.less'),
-    src('src/less/editor-style.less')
-  )
+  return src(['src/less/style.less', 'src/less/editor-style.less'])
   .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
   .pipe(less())
   .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
   .pipe(gulpif(PRODUCTION, cleanCss({compatibility: 'ie8'})))
   .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
   .pipe(dest('./'))
+  .pipe(server.stream());
 }
 
 export const watchForChanges = () => {
   watch('src/less/**/*.less', styles);
-  watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', images);
-  watch(['src/**/*','!src/{images,js,less}','!src/{images/js/less}/**/*']);
-  watch('src/js/**/*.js', scripts);
+  watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', series(images, reload));
+  watch(['src/**/*','!src/{images,js,less}','!src/{images/js/less}/**/*'], series(copy, reload));
+  watch('src/js/**/*.js', series(scripts, reload));
+  watch('**/*.php', reload);
 }
 
 export const images = () => {
   return src('src/images/**/*.{jpg,jpeg,png,svg,gif}')
     .pipe(gulpif(PRODUCTION, imagemin()))
-    .pipe(dest('./images'));
+    .pipe(dest('./dist/images'));
 }
 
 export const scripts = () => {
@@ -65,9 +66,50 @@ export const scripts = () => {
       jquery: 'jQuery'
     },
   }))
-  .pipe(dest('./js'));
+  .pipe(dest('./dist/js'));
 }
 
-export const dev = series(parallel(styles, images, scripts), watchForChanges);
-export const build = series(parallel(styles, images, scripts));
+export const copy = () => {
+  return src(['src/**/*','!src/{images,js,less}','!src/{images,js,less}/**/*'])
+    .pipe(dest('dist'));
+}
+
+export const clean = () => {
+  return del(['dist']);
+}
+
+const server = browserSync.create();
+export const serve = done => {
+  server.init({
+    proxy: "http://dev.goshawk-starter.com" // put your local website link here
+  });
+  done();
+};
+export const reload = done => {
+  server.reload();
+  done();
+};
+
+export const compress = () => {
+  return src([
+    "**/*",
+    "!node_modules{,/**}",
+    "!src{,/**}",
+    "!.babelrc",
+    "!.gitignore",
+    "!gulpfile.babel.js",
+    "!package.json",
+    "!package-lock.json",
+  ])
+  .pipe(
+    gulpif(
+      file => file.relative.split(".").pop() !== "zip",
+      replace('_themename', info.name)
+    )
+  .pipe(zip(`${info.name}.zip`))
+  .pipe(dest('bundled'));
+}
+
+export const dev = series(clean, parallel(styles, images, scripts, copy), serve, watchForChanges);
+export const build = series(clean, parallel(styles, images, copy, scripts), compress);
 export default dev;
