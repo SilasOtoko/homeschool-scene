11b7d1da25339f6d142d715a300c13e00aac643f
Josiah Schaefer <josiah+gitlab@rarebirdinc.com>
2019-06-15 13:38:05 -0400

    trying to get Browsersync working

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,16 @@
 const mix = require('laravel-mix');
-const BabelMinifyPlugin = require('babel-minify-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 mix.js('src/js/bundle.js', 'dist/js/bundle.js')
    .less('src/less/style.less', 'style.css')
    .copyDirectory('src/images', 'dist/images')
    .options({
      processCssUrls: false,
-   });
+   })
+   .browserSync({
+    proxy: 'dev.goshawk-starter.com',
+    files: [
+      'src/less/style.less',
+      'src/js/bundle.js'
+    ]
+  });
