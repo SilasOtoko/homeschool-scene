7e4538283c741bcedf210efc1adeff8b8908c59c
Josiah Schaefer <josiah+gitlab@rarebirdinc.com>
2019-06-17 10:37:19 -0400

    added sourcemaps to mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 mix.js('src/js/bundle.js', 'dist/js/bundle.js')
    .less('src/less/style.less', 'style.css')
+   .sourceMaps()
    .copyDirectory('src/images', 'dist/images')
    .options({
      processCssUrls: false,
@@ -17,4 +18,8 @@ mix.browserSync({
     'src/js/{*,**/*}.js',
     '{*,**/*}.php'
   ],
+});
+
+mix.webpackConfig({
+  devtool: "inline-source-map"
 });
