9eb8cf20c6f6138219dcd20e80f18ebec05d4f6b
Josiah Schaefer <josiah@rarebirdinc.com>
2020-06-01 10:58:59 -0400

    updating theme styling after Parker project

--- index.php
+++ index.php
@@ -40,72 +40,76 @@
 
         <?php endif; ?>
 
-        <div class="main-content-row row post-list">
+        <div class="post-list">
 
-            <div class="main-content-column content-column">
-                <section class="main-content">
+            <div class="main-content-row row">
 
-                    <?php
-                        if ( have_posts() ) :
+                <div class="main-content-column content-column">
+                    <section class="main-content">
 
-                            /* Start the Loop */
-                            while ( have_posts() ) :
-                                the_post();
+                        <?php
+                            if ( have_posts() ) :
 
-                                /*
-                                 * Include the Post-Type-specific template for the content.
-                                 * If you want to override this in a child theme, then include a file
-                                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-                                 */
-                                get_template_part( 'template-parts/content/content', get_post_type() );
+                                /* Start the Loop */
+                                while ( have_posts() ) :
+                                    the_post();
 
-                            endwhile; ?>
+                                    /*
+                                     * Include the Post-Type-specific template for the content.
+                                     * If you want to override this in a child theme, then include a file
+                                     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                                     */
+                                    get_template_part( 'template-parts/content/content', get_post_type() );
 
-                            <nav class="navigation pagination" role="navigation">
-                                <h2 class="screen-reader-text">Posts navigation</h2>
-                                <div class="nav-links">
+                                endwhile; ?>
 
-                                    <?php 
-                                        $big = 999999999; // need an unlikely integer
-                                        $translated = __( 'Page', 'mytextdomain' );
-                                        global $wp_query;
-                                        $total = $wp_query->max_num_pages;
-                                    ?>
+                                <nav class="navigation pagination" role="navigation">
+                                    <h2 class="screen-reader-text">Posts navigation</h2>
+                                    <div class="nav-links">
 
-                                    <?php $argsp = array(
-                                        'base'               => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-                                        'format'             => '?paged=%#%',
-                                        'total'              => $total,
-                                        'current'            => max( 1, get_query_var('paged') ),
-                                        'prev_next'          => true,
-                                        'prev_text'          => __('«'),
-                                        'next_text'          => __('»'),
-                                        'type'               => 'plain',
-                                        'add_args'           => false,
-                                        'add_fragment'       => '...',
-                                        'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>',
-                                        'after_page_number'  => ''
-                                    );
+                                        <?php 
+                                            $big = 999999999; // need an unlikely integer
+                                            $translated = __( 'Page', 'mytextdomain' );
+                                            global $wp_query;
+                                            $total = $wp_query->max_num_pages;
+                                        ?>
+
+                                        <?php $argsp = array(
+                                            'base'               => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                                            'format'             => '?paged=%#%',
+                                            'total'              => $total,
+                                            'current'            => max( 1, get_query_var('paged') ),
+                                            'prev_next'          => true,
+                                            'prev_text'          => __('«'),
+                                            'next_text'          => __('»'),
+                                            'type'               => 'plain',
+                                            'add_args'           => false,
+                                            'add_fragment'       => '...',
+                                            'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>',
+                                            'after_page_number'  => ''
+                                        );
 
 
-                                    echo paginate_links( $argsp );
-                                    ?>
+                                        echo paginate_links( $argsp );
+                                        ?>
 
-                                </div>
+                                    </div>
 
-                            </nav>
+                                </nav>
 
-                        <?php else :
+                            <?php else :
 
-                            get_template_part( 'template-parts/content/content', 'none' );
+                                get_template_part( 'template-parts/content/content', 'none' );
 
-                        endif;
-                    ?>
+                            endif;
+                        ?>
 
-                </section>
+                    </section>
+                </div>
+
+                <?php get_sidebar(); ?>
+
             </div>
-
-            <?php get_sidebar(); ?>
 
         </div>
 
