eeec8a639c5fa69252a574aa71e726a7647ead64
Josiah Schaefer <josiah@rarebirdinc.com>
2020-07-24 23:14:39 -0400

    running in production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,21 @@
 const mix = require('laravel-mix');
 
+mix.setPublicPath('.');
+
 mix.js('src/js/bundle.js', 'dist/js/bundle.js')
    .less('src/less/style.less', 'style.css')
    .less('src/less/editor-style.less', 'editor-style.css')
-   .sourceMaps()
    .copyDirectory('src/images', 'dist/images')
    .options({
      processCssUrls: false,
    });
 
-mix.webpackConfig({
-  devtool: "inline-source-map"
-});
-
 if (mix.inProduction()) {
     mix.version();
+} else {
+    mix
+        .webpackConfig({
+          devtool: "inline-source-map"
+        })
+        .sourceMaps();
 }
